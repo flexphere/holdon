d600cd69e96d2bba0b24117a126c12efb9e906f6
DJahvoe <dnaspeedmania@gmail.com>
2021-06-23 15:37:37 +0700

    push push

--- app/main.js
+++ app/main.js
@@ -39,7 +39,10 @@ function createWindow () {
   });
   
   win.loadFile('app/index.html');
-  win.webContents.openDevTools();
+  if(process.env.NODE_ENV === 'dev')
+  {
+    win.webContents.openDevTools();
+  }
   win.webContents.send("clipboardHistoryUpdated", history.data);
 
   win.on('blur', function(event){
